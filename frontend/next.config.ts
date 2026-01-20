6cd12cc1ab88509367fc0a960cccab20fee8ee1a
Thewind777 <firas.abuzakhar@studenti.luiss.it>
2026-01-20 22:38:48 +0200

    fix: update next config and prep for root deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "m.media-amazon.com" },
+      { protocol: "https", hostname: "www.themealdb.com" },
+      { protocol: "https", hostname: "foodish-api.com" },
+      { protocol: "https", hostname: "*.supabase.co" },
+    ],
+  },
 };
 
 export default nextConfig;
